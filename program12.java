419d1316c5152a35d463be4e2e89ce4ed9a88de8
PRANAV PARATE <pranavparate45@gmail.com>
2026-01-06 21:30:26 +0530

    Add files via upload
    Signed-off-by: PRANAV PARATE <pranavparate45@gmail.com>

---
+++ program12.java
@@ -0,0 +1,16 @@
+public class program2 {
+    public static void main (String[]args){
+        int [] marks = new int [3];
+
+        marks [0] = 23;
+        marks [1] = 42;
+        marks [2] = 32;
+        System.out.println(marks[0]); 
+        System.out.println(marks[1]); 
+        System.out.println(marks[2]); 
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
---
+++ program13.java
@@ -0,0 +1,13 @@
+
+public class program1 {
+    public static void main(String[] args) {
+        // time & space Complexity
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        int Array[] = { 2, 3, 4, 5, 7 };
+        int n = Array.length;
+        for (int i = 0; i < n; i++) {
+            System.out.println(n + "Hello");
+        }
+    }
+}
